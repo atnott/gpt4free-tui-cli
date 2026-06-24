5af780cfbc35a4683448626888f12df828961f21
Maximka Sorokin <maks256maksik@gmail.com>
2026-06-24 17:23:43 +0800

    feat(tui): add input logic and change styles

--- src/interface/tui_textual.py
+++ src/interface/tui_textual.py
@@ -12,13 +12,30 @@ class ExperementalTUI(App):
             Placeholder(id = "Models_window"),
             Container(
                 RichLog(id = "chat_log"),
-                Input(id = "user_input"),
+                Input(placeholder = "Input your text here...", id = "user_input"),
                 id = "Chat_window"
             ),
             Placeholder(id = "Providers_window"),
             id = "Main_container"
         )
         yield Footer(id = "TUI_footer")
+
+    def on_mount(self) -> None:
+        self.theme = "textual-dark"
+        
+        self.query_one("#Chat_window").border_title = "Chat"
+
+
+    #Ввод пользователя
+    def on_input_submitted(self, event: Input.Submitted) -> None:
+        user_text = event.value.strip()
+
+        if not user_text:
+            return
+
+        chat_log = self.query_one("#chat_log", RichLog)
+        chat_log.write(f"User: {user_text}")
+        event.input.value = ""
 
 
 if __name__ == "__main__":
