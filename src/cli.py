f30a4535fe88cdc0fb786e9a497453c4b2566b06
anton krotov <twinkidr@gmail.com>
2026-06-25 21:08:00 +0800

    feat: implement models and providers commands

--- src/cli.py
+++ src/cli.py
@@ -4,6 +4,7 @@ from core.engine import G4FEngine
 from rich.markdown import Markdown
 from rich.console import Console
 from core.config import ConfigManager
+from rich.table import Table
 
 app = typer.Typer(help='GPT4FREE Terminal Client')
 engine = G4FEngine()
@@ -43,6 +44,34 @@ def main(
         asyncio.run(stream_response(chosen_model, chosen_provider, prompt))
     else:
         typer.echo(f'tui')
+
+@app.command(name='models')
+def list_models() -> None:
+    '''Вывести список всех уникальных моделей от работающих провайдеров'''
+    models = engine.get_all_models()
+
+    table = Table(title=f'Доступно моделей: {len(models)}шт')
+    table.add_column('Имя модели (-m)')
+
+    for model in models:
+        table.add_row(model)
+    console.print(table)
+
+
+@app.command(name='providers')
+def list_providers() -> None:
+    '''Вывести список всех работающих провайдеров и их моделей'''
+    providers_status = engine.get_available_providers()
+
+    table = Table(title=f'Работающие провайдеры ({len(providers_status)}шт)', show_lines=True)
+    table.add_column('Провайдер (-pr)')
+    table.add_column('Поддерживаемые модели')
+
+    for provider in providers_status:
+        models_str = ', '.join(provider.supported_models)
+        table.add_row(provider.name, models_str)
+
+    console.print(table)
 
 if __name__ == '__main__':
     app()
