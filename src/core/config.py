474a52c753893df990e40ed90c423ca5ac2d23c5
anton krotov <twinkidr@gmail.com>
2026-06-25 20:01:51 +0800

    feat: implement ConfigManager for implicit user settings storage

---
+++ src/core/config.py
@@ -0,0 +1,40 @@
+import json
+from pathlib import Path
+from typing import Any
+
+class ConfigManager:
+    '''Класс для управления сохранением настроек пользователя'''
+    def __init__(self, filename: str = 'config.json') -> None:
+        self.config_dir = Path.home() / '.config' / 'gpt4free-tui-cli'
+        self.config_path = self.config_dir / filename
+
+        self.default_config: dict[str, Any] = {
+            'last_model': 'gpt-4o',
+            'last_provider': None
+        }
+
+    def _ensure_config_exists(self) -> None:
+        '''проверяет наличие папки и файла, если их нет - создает с дефолтными настройками'''
+        self.config_dir.mkdir(parents=True, exist_ok=True)
+        if not self.config_path.exists():
+            with open(self.config_path, 'w', encoding='utf-8') as f:
+                json.dump(self.default_config, f, indent=4, ensure_ascii=False)
+
+    def load_config(self) -> dict[str, Any]:
+        '''Читает конфиг с диска. Если файла нет, сначала создает его'''
+        self._ensure_config_exists()
+        with open(self.config_path, 'r', encoding='utf-8') as f:
+            return json.load(f)
+
+    def update_config(self, model: str, provider: str | None = None) -> None:
+        '''Перезаписывает настройки в файле'''
+        self._ensure_config_exists()
+        with open(self.config_path, 'w', encoding='utf-8') as f:
+            new_config = dict(last_model=model, last_provider=provider)
+            json.dump(new_config, f, indent=4, ensure_ascii=False)
+
+if __name__ == '__main__':
+    manager = ConfigManager()
+    print(manager.load_config())
+    manager.update_config(model="qwen-2.5", provider="HuggingFace")
+    print(manager.load_config())
