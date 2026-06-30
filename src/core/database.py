948e6b8bb39c6ffb98f7a9aced37b995e1e7c652
anton krotov <twinkidr@gmail.com>
2026-06-30 17:29:37 +0800

    feat: implement database initialization and core tables setup

--- src/core/database.py
+++ src/core/database.py
@@ -5,11 +5,36 @@ class DatabaseManager:
     def __init__(self) -> None:
         self.path_dir = Path(__file__).resolve().parent.parent.parent
         self.db_path = self.path_dir / 'storage.db'
+        self._init_db()
 
     def _get_connection(self):
+        '''Возвращает соединение с базой данных'''
         conn = sqlite3.connect(self.db_path)
         conn.execute("PRAGMA foreign_keys = ON;")
         conn.row_factory = sqlite3.Row
         return conn
 
+    def _init_db(self) -> None:
+        '''Создает таблицы в базе данных и инициализирует дефолтный чат'''
+        with self._get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute('''
+            CREATE TABLE IF NOT EXISTS chats (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );''')
+            cursor.execute('''
+            CREATE TABLE IF NOT EXISTS messages (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                chat_id INTEGER NOT NULL,
+                role TEXT NOT NULL,
+                content TEXT NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (chat_id) REFERENCES chats (id) ON DELETE CASCADE
+            );''')
+            cursor.execute('''
+            INSERT OR IGNORE INTO chats (id, title)
+            VALUES (1, 'Основной диалог')
+            ''')
 
