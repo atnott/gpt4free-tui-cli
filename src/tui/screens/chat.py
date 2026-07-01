2e54a0e02d9d0f88b3209fa7508c127927bdb1db
Maximka Sorokin <maks256maksik@gmail.com>
2026-07-01 13:56:05 +0800

    refactor(TUI): change chat screen

--- src/tui/screens/chat.py
+++ src/tui/screens/chat.py
@@ -1,22 +1,20 @@
-from textual.screen import Screen
+from textual.app import ComposeResult
 from textual.containers import Horizontal, Vertical
-from textual.widgets import Placeholder
-from tui.widgets.chat_log import ChatLog
+from textual.screen import Screen
 from tui.widgets.chat_input import ChatInput
+from tui.widgets.chat_log import ChatLog
+from tui.widgets.info_panel import InfoPanel
+
 
 class ChatScreen(Screen):
-    def compose(self):
-        """Экран чата сайдбар с настройками и чат с логом"""
+
+    def compose(self) -> ComposeResult:
         with Horizontal():
-            yield Vertical(
-                Placeholder(),
-                id="sidebar_panel") 
+            with Vertical(id="sidebar_panel"):
+                pass
             
             with Vertical(id="chat_panel"):
                 yield ChatLog(id="chat_log")
                 yield ChatInput(id="chat_input")
 
-            yield Vertical(
-                Placeholder(),
-                id = "info_panel"
-            )
+            yield InfoPanel(id="info_panel")
